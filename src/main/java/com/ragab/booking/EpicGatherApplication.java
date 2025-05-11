a488b494caf25bc90b95ff34d7ce54bbd4f20a39
MohammedRagab00 <Mohammed_Ragab_@outlook.com>
2025-05-11 13:28:40 +0300

    first commit

---
+++ src/main/java/com/ragab/booking/EpicGatherApplication.java
@@ -0,0 +1,13 @@
+package com.ragab.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicGatherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EpicGatherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ragab/booking/EpicGatherApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ragab.booking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EpicGatherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
